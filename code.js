cebf961e25b48ef6a6baaf8ef560f2c1de094de0
TIMSTRICK <122310167+TIMSTRICK@users.noreply.github.com>
2023-03-09 01:06:38 +0000

    Made changes and moved some functions around

--- code.js
+++ code.js
@@ -1,36 +1,11 @@
-function play() {
-    // generate a random number between 1 and 6 for the first die
-    var die1 =Math.ceil(Math.random()*6);
-    
-    // generate a random number between 1 and 6 for the second die
-    var die2 =Math.ceil(Math.random()*6);
-    
-    //calculate the sum of the two die values
-    var sum = die1+die2
-    
-    //write the value of each die and the sum to the document
-    document.write("Die 1 = " + die1) 
-    document.write("<br/>") 
-    document.write("Die 2 = " + die2) 
-    document.write("<br/>") 
-    document.write("Sum = " + sum)
-    document.write("<br/>") 
 
-    // check if the sum is equal to 7 or 11
-    if (sum == 7 || sum == 11){ 
-    
-        // if the sum is 7 or 11, write "CRAPS - you lose" to the document
-        document.write("CRAPS - you lose") 
-        document.write("<br/>") 
-    }
-    // check if the value of the two dice are equal and even
-    else if (die1== die2 && die1%2 == 0){
-        // if the value of the two dice are equal and even, write "DOUBLES - you win" to the document
-        document.write("DOUBLES - you win") 
-        document.write("<br/>") 
-    }
-    
-}
+/**
+* Delay for a number of milliseconds
+*/
+function sleep(delay) {
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+  }
 
 function startBlastoff() {   
     // Initialize the currTime variable to 50
@@ -74,8 +49,52 @@ function stopButtonClicked() {
     document.getElementById("start_button").disabled = false;
 }
 
-function playDice() {
-    var audio = new Audio('');
+// This func is to play a sound. 
+
+function play() { // One day I might make this have multiple different sounds that one can pass in an arg to get the proper sound. 
+    var audio = new Audio('dice-rolling-on-table.wav');
     audio.play();
   }
 
+  function playGame() {
+      console.log("Running the Game!");
+      // generate a random number between 1 and 6 for the first die
+      var die1 =Math.ceil(Math.random()*6);
+      
+      // generate a random number between 1 and 6 for the second die
+      var die2 =Math.ceil(Math.random()*6);
+      
+      //calculate the sum of the two die values
+      var sum = die1+die2
+      
+      document.getElementById('game').innerHTML = " ";
+
+      //write the value of each die and the sum to the document
+      document.getElementById('game').innerHTML += "Die 1 = " + die1 + "";
+      document.getElementById('game').innerHTML += "<br/>";
+      document.getElementById('game').innerHTML += "Die 2 = " + die2 + ""; 
+      document.getElementById('game').innerHTML += "<br/>"; 
+      document.getElementById('game').innerHTML += "Sum = " + sum + "";
+      document.getElementById('game').innerHTML += "<br/>"; 
+
+
+      // check if the sum is equal to 7 or 11
+      if (sum == 7 || sum == 11){ 
+      
+          // if the sum is 7 or 11, write "CRAPS - you lose" to the document
+          document.getElementById('game').innerHTML += "CRAPS - you lose";
+          document.getElementById('game').innerHTML += "<br/>";
+      }
+      // check if the value of the two dice are equal and even
+      else if (die1 == die2 && die1%2 == 0){
+          // if the value of the two dice are equal and even, write "DOUBLES - you win" to the document
+          document.getElementById('game').innerHTML += "DOUBLES - you win";
+          document.getElementById('game').innerHTML += "<br/>";
+      }
+  }
+
+    // This function is used to call 2 funcs for craps.html
+  function rollDice() {
+      play();
+      playGame();  
+  }
